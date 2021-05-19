376619fc146a52e32932e8a1da730db84ab186da
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-20 01:56:52 +0800

    Add print example

---
+++ src/print.rs
@@ -0,0 +1,6 @@
+pub fn run(){
+    println!("Hello printrs");
+    println!("num: {}", 1);
+    println!("{0} is {1} but {0} is not {2}", "x", 0, 1);
+    println!("{name} is a good {job}", name="Gibbs", job="engineer")
+}
