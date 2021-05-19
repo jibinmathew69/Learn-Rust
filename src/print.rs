87f43a899e7b132933d30bf4e698acdad5e887c1
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-20 02:09:20 +0800

    Add print debig traits

--- src/print.rs
+++ src/print.rs
@@ -4,4 +4,5 @@ pub fn run(){
     println!("{0} is {1} but {0} is not {2}", "x", 0, 1);
     println!("{name} is a good {job}", name="Gibbs", job="engineer");
     println!("{:b} {:o} {:x}", 10, 10, 10);
+    println!("{:?}", (1, true, "asdf"))
 }
