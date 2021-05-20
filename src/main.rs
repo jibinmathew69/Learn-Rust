87afdc0c592b76a29e3b5567126660719faaf87e
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-20 16:23:14 +0800

    Add vectors

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 // mod vars;
 // mod strings;
 // mod tuples;
-mod arrays;
+// mod arrays;
+mod vectors;
 
 fn main() {
     // print::run();
     // vars::run();
     // strings::run();
     // tuples::run();
-    arrays::run();
+    // arrays::run();
+    vectors::run();
 }
---
+++ src/vectors.rs
@@ -0,0 +1,18 @@
+use std::mem;
+
+pub fn run(){
+    let mut numbers: Vec<i32> = vec![1, 2, 2, 3, 3];
+
+    println!("{:?}", numbers);
+    println!("{}", numbers[2]);
+
+    numbers[2] = 3;
+
+    println!("{:?}", numbers);
+    println!("{}", numbers.len());
+
+    println!("{}", mem::size_of_val(&numbers));
+
+    let slice: &[i32] = &numbers[1..3];
+    println!("{:?}", slice);
+}
