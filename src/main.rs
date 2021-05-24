aeba480d50040478cf99f6ff7f9fa24b2173bf19
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-24 15:18:53 +0800

    Add pointers

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
 // mod vectors;
 // mod conditionals;
 // mod loops;
-mod functions;
+// mod functions;
+mod pointer_ref;
 
 fn main() {
     // print::run();
@@ -17,5 +18,6 @@ fn main() {
     // vectors::run();
     // conditionals::run();
     // loops::run();
-    functions::run();
+    // functions::run();
+    pointer_ref::run();
 }
