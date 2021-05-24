c3bda7b20589a20cbb077359ac7e5d1820ba0316
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-24 14:40:08 +0800

    Add functions closures

--- src/functions.rs
+++ src/functions.rs
@@ -2,6 +2,9 @@ pub fn run(){
     greeting("Hello", "Gibbs");
     let getch = add(5, 6);
     println!("{}", getch);
+
+    let add_nums = |n1: i32, n2: i32| n1+n2;
+    println!("{}", add_nums(3, 4)); 
 }
 
 fn greeting(greet: &str, name: &str){
