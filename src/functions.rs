6064ec5436b0f4350af9584d5c891cc59663d626
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-24 14:37:56 +0800

    Add functions returns

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,13 @@
 pub fn run(){
     greeting("Hello", "Gibbs");
+    let getch = add(5, 6);
+    println!("{}", getch);
 }
 
 fn greeting(greet: &str, name: &str){
     println!("{}, {}", greet, name);
+}
+
+fn add(num1: i32, num2: i32) -> i32{
+    num1 + num2
 }
